fa622a32e6fd661cc14be1621c3136eb79c77dd4
Anouk <anouk.paulino@epfl.ch>
2023-03-02 12:05:24 +0100

    completion of fragment + navigation view

--- app/src/main/java/com/github/romainlogean/sdp_bootcamp/MainActivity.java
+++ app/src/main/java/com/github/romainlogean/sdp_bootcamp/MainActivity.java
@@ -2,13 +2,23 @@ package com.github.romainlogean.sdp_bootcamp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,25 +28,28 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.mainGoButton);
         button.setOnClickListener(this::greet);
 
-        /*Button options = findViewById(R.id.optionsButton);
-        options.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.fragmentContainerOptions, DisplayFragment.class, null)
-                        .setReorderingAllowed(true)
-                        .addToBackStack("name") // whatever name ?
-                        .commit();
-            }
-        });*/
+        // create toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
+        setSupportActionBar(toolbar);
 
+        drawerLayout = findViewById(R.id.main_drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navOptions);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        // add fragment to main view
-        /*getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.fragmentContainerOptions, new DisplayFragment())
-                .commit();*/
+        // enable toggling options
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // default sets to Forum Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new ForumFragment())
+                    .commit();
+            navigationView.setCheckedItem(R.id.forumMenuItem);
+        }
     }
 
     public void greet(View view){
@@ -47,11 +60,42 @@ public class MainActivity extends AppCompatActivity {
         view.getContext().startActivity(intent);
     }
 
-    public void toggleOptions(View view){
-        Intent intent = new Intent(view.getContext(), GreetingActivity.class);
-        TextView nameTV = findViewById(R.id.mainName);
-        String name = nameTV.getText().toString();
-        intent.putExtra("NAME", name);
-        view.getContext().startActivity(intent);
+    // TO DO
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.forumMenuItem:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, new ForumFragment())
+                        .commit();
+                break;
+
+            case R.id.chatMenuItem:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, new ChatFragment())
+                        .commit();
+                break;
+
+            case R.id.savedMenuItem:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, new SavedFragment())
+                        .commit();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
