3baee1b565d851bf001f7f17eb7e6561dbeb0f72
Anouk <anouk.paulino@epfl.ch>
2023-03-08 16:44:28 +0100

    strill

--- app/src/androidTest/java/com/github/ybecker/epforuml/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/ybecker/epforuml/MainActivityTest.kt
@@ -15,9 +15,4 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class MainActivityTest {
-    @get:Rule
-    val testRule = createAndroidComposeRule(MainActivity::class.java)
-
-
-}
+class MainActivityTest {}
