4f075fd5ed6241f1f0c7f5f77c058608c08161cf
unknown <luna.godier@epfl.ch>
2023-03-09 17:08:17 +0100

    added login ui

---
+++ app/src/main/java/com/github/ybecker/epforuml/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.github.ybecker.epforuml.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.github.ybecker.epforuml.data.LoginDataSource
+import com.github.ybecker.epforuml.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
