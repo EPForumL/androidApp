97fa80cf3dc994144a523eca837581073cafd3e5
unknown <luna.godier@epfl.ch>
2023-03-12 17:30:01 +0100

    simple ui for login

---
+++ app/src/main/java/com/github/ybecker/epforuml/login/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.github.ybecker.epforuml.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        }
+    }
