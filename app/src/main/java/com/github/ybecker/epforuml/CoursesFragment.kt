ce6391c0e88d0e060194369600bf713fc3551497
RomainLogean <rom1logean@gmail.com>
2023-03-13 20:02:43 +0100

    first course fragement implementation

--- app/src/main/java/com/github/ybecker/epforuml/CoursesFragment.kt
+++ app/src/main/java/com/github/ybecker/epforuml/CoursesFragment.kt
@@ -5,18 +5,79 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.github.ybecker.epforuml.database.DatabaseManager
+import com.github.ybecker.epforuml.database.Model
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CoursesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CoursesFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_courses, container, false)
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CourseAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_courses, container, false)
+        recyclerView = rootView.findViewById(R.id.recyclerViewCourses)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+
+        //TODO CHANGE
+        DatabaseManager.useMockDatabase()
+        val db = DatabaseManager.getDatabase()
+
+        adapter = CourseAdapter(db.availableCourses().toList()) { course ->
+            onCourseClick(course)
+        }
+        recyclerView.adapter = adapter
+        return rootView
     }
+
+    private inner class CourseAdapter(
+        private val courses: List<Model.Course>,
+        private val onCourseClickListener: (Model.Course) -> Unit
+    ) : RecyclerView.Adapter<CourseViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_course, parent, false)
+            return CourseViewHolder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
+            val course = courses[position]
+            holder.bind(course)
+            holder.itemView.setOnClickListener { onCourseClickListener(course) }
+        }
+
+        override fun getItemCount() = courses.size
+
+    }
+
+    private inner class CourseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val courseTitleTextView = itemView.findViewById<TextView>(R.id.courseTitleTextView)
+
+        fun bind(course: Model.Course) {
+            courseTitleTextView.text = course.courseName
+        }
+    }
+
+    private fun onCourseClick(course: Model.Course) {
+        println("Clicked course: ${course.courseName}")
+
+        launchCourseQuestionFragment(course.courseName)
+    }
+
+    private fun launchCourseQuestionFragment(courseName: String) {
+        val courseQuestionFragment = CourseQuestionsFragment()
+        val bundle = Bundle()
+        bundle.putString("courseName", courseName)
+        courseQuestionFragment.arguments = bundle
+
+        val fragmentManager = parentFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.courses_layout_parent, courseQuestionFragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
 }
