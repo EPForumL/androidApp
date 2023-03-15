3a849cae5dbb93500fd4ca647a4a574cb7472028
Anouk <anouk.paulino@epfl.ch>
2023-03-15 17:46:40 +0100

    recyclerView in ForumFragment

--- app/src/main/java/com/github/ybecker/epforuml/HomeFragment.kt
+++ app/src/main/java/com/github/ybecker/epforuml/HomeFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.github.ybecker.epforuml.database.FirebaseDatabaseAdapter
+import com.github.ybecker.epforuml.database.Model
 
 /**
  * A simple [Fragment] subclass.
@@ -12,11 +16,49 @@ import android.view.ViewGroup
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
+
+    private lateinit var adapter : ForumAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var questionsList : MutableList<String> // switch to questions when able to transfer data from mainActivtiy
+
+    /*
+    lateinit var questions = Array<String> ??? // is this needed ?
+     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    /**
+     * Initialize question array
+     */
+    private fun questionsRetrieval() {
+        questionsList = mutableListOf(
+            "What about me ?",
+            "What about us ?",
+            "An especially looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong question because" +
+                    "we need to be able to test it and for now we have not had another way of testing it iti it " +
+                    "it it it it it it it it it it it it it it it it it it it it it it it it it it it " +
+                    "it it it it it it it it it it it it it it it it it it it it it it it it it it it " +
+                    "it it it it it it it it it it it it it it it it it it it it it it it it it it it it it ",
+            "last question I promise"
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // remove when using mockDB
+        questionsRetrieval()
+
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView = view.findViewById(R.id.recycler_forum)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true) // maybe change that later
+
+        adapter = ForumAdapter(questionsList)
+        recyclerView.adapter = adapter
     }
 }
--- app/src/main/java/com/github/ybecker/epforuml/MainActivity.kt
+++ app/src/main/java/com/github/ybecker/epforuml/MainActivity.kt
@@ -6,15 +6,26 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import com.github.ybecker.epforuml.database.Database
+import com.github.ybecker.epforuml.database.DatabaseManager
+import com.github.ybecker.epforuml.database.MockDatabase
+import com.github.ybecker.epforuml.database.Model
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle : ActionBarDrawerToggle
     lateinit var drawerLayout: DrawerLayout
+    private lateinit var db : Database
+    lateinit var bundle : Bundle
+
+    private var questionsList = mutableListOf<Model.Question>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // initialize DB to Mock
+        DatabaseManager.useMockDatabase()
 
         drawerLayout = findViewById(R.id.drawer_layout)
         val navView : NavigationView = findViewById(R.id.nav_view)
@@ -27,6 +38,13 @@ class MainActivity : AppCompatActivity() {
 
         if(savedInstanceState == null) {
             supportFragmentManager.beginTransaction().replace(R.id.frame_layout, HomeFragment()).commit()
+        }
+
+
+        // find a way of transfering content of db to fragments
+        getQuestionsQuery()
+        for (question in questionsList) {
+            bundle.putString(question.questionId, question.questionText)
         }
 
 
@@ -56,5 +74,16 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction().replace(R.id.frame_layout, fragment).commit()
         drawerLayout.closeDrawers()
         setTitle(title)
+    }
+
+    fun getQuestionsQuery() {
+        var courseSet = db.availableCourses()
+
+        // reset questionsList for refresh
+        questionsList = mutableListOf()
+
+        for (course in courseSet) {
+            questionsList.addAll(db.getCourseQuestions(course))
+        }
     }
 }
