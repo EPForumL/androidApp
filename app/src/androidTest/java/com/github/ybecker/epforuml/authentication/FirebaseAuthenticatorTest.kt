1a8c12f7b39d96a918289c5a1937b1491afedf16
Yann <yann.becker@epfl.ch>
2023-03-16 16:29:32 +0100

    Rework FirebaseAuthenticator.kt, AuthenticatorManager.kt and add MockAuthenticator.kt
    Move back onSignInResult and signInLauncher inside FirebaseAuthenticator.kt as it makes more sense. Also code refactor for onSignInResult as it was too big.
    I also added a MockAuthenticator for testing.

--- app/src/androidTest/java/com/github/ybecker/epforuml/authentication/FirebaseAuthenticatorTest.kt
+++ app/src/androidTest/java/com/github/ybecker/epforuml/authentication/FirebaseAuthenticatorTest.kt
@@ -2,19 +2,40 @@ package com.github.ybecker.epforuml.authentication
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.github.ybecker.epforuml.MainActivity
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import com.github.ybecker.epforuml.R
+import org.hamcrest.Matchers.*
+import org.junit.After
+import org.junit.Before
 
 
 @RunWith(AndroidJUnit4::class)
 class FirebaseAuthenticatorTest {
-    //@Rule
-    //val testRule = ActivityScenarioRule(MainActivity::class.java)
+    @get:Rule
+    val testRule = ActivityScenarioRule(LoginActivity::class.java)
+
+    @Before
+    fun initIntents() {
+        Intents.init()
+    }
+
+    @After
+    fun releaseIntents() {
+        Intents.release()
+    }
 
     @Test
-    fun test() {
-
+    fun checkSignInButtonOpensSignInActivity() {
+        onView(ViewMatchers.withId(R.id.signInButton)).perform(click())
+        onView(ViewMatchers.withText("Sign in with Google")).check(matches(isDisplayed()))
+        onView(ViewMatchers.withText("Sign in with email")).check(matches(isDisplayed()))
     }
 }
