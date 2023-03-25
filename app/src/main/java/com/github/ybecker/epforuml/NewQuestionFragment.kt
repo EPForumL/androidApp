4fe9c00811991642b01814da366202946b18d5f8
unknown <luna.godier@epfl.ch>
2023-03-25 17:08:39 +0100

    performed some mantainability modifs

--- app/src/main/java/com/github/ybecker/epforuml/NewQuestionFragment.kt
+++ app/src/main/java/com/github/ybecker/epforuml/NewQuestionFragment.kt
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment
 import com.github.ybecker.epforuml.authentication.AuthenticatorManager
 import com.github.ybecker.epforuml.database.DatabaseManager
 import com.github.ybecker.epforuml.database.DatabaseManager.db
+import com.github.ybecker.epforuml.database.Model
 
 /**
  * A simple [Fragment] subclass.
@@ -21,10 +22,6 @@ import com.github.ybecker.epforuml.database.DatabaseManager.db
 class NewQuestionFragment(val mainActivity: MainActivity) : Fragment() {
 
     private lateinit var takePictureButton: Button
-
-
-
-
     var IMAGE_URI = ""
     private val pickImage =
         registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
@@ -36,123 +33,130 @@ class NewQuestionFragment(val mainActivity: MainActivity) : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-
         // DataBase
-
         // Create an instance of the MockDatabase
         //val mockDatabase = DatabaseManager.useMockDatabase()
-
         //user
         val user = AuthenticatorManager.authenticator?.user
 
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_new_question, container, false)
 
-
         //Spinner
-
         // Retrieve the Spinner view
         val spinner = view.findViewById<Spinner>(R.id.subject_spinner)
-        //spinner.se = this.mainActivity.intent.getStringExtra("subject")
 
         // Get the set of available courses from the MockDatabase
         db.availableCourses().thenAccept {
             val coursesList = it
-
             // Convert the set to an ArrayList
             //val coursesList = ArrayList(coursesSet)
-
             val courseNamesList = coursesList.map { course -> course.courseName }
-
             // Create an ArrayAdapter with the coursesList as the data source
             val adapter = ArrayAdapter(
                 requireContext(),
                 android.R.layout.simple_spinner_dropdown_item,
                 courseNamesList
             )
-
             // Set the ArrayAdapter as the Spinner adapter
             spinner.adapter = adapter
 
             //set if not empty
+            val (questBody, questTitle, imageURI) = setUp(view)
 
-            val questBody = view.findViewById<EditText>(R.id.question_details_edittext)
-            val questTitle = view.findViewById<EditText>(R.id.question_title_edittext)
-            val imageURI = view.findViewById<TextView>(R.id.image_uri)
-
-            questBody.setText(this.mainActivity.intent.getStringExtra("questionDetails"))
-            questTitle.setText(this.mainActivity.intent.getStringExtra("questionTitle"))
-            imageURI.setText(this.mainActivity.intent.getStringExtra("uri"))
-
-            // perform submit action
-            //SubmitButton
-
-            val submitButton = view?.findViewById<Button>(R.id.btn_submit)
-            submitButton?.setOnClickListener {
-                print("click submit btn")
-
-                if (questBody.text.isBlank() || questTitle.text.isBlank()) {
-                    Toast.makeText(
-                        requireContext(),
-                        "Question title or body cannot be empty",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                } else {
-                    spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                        override fun onItemSelected(
-                            parent: AdapterView<*>,
-                            view: View?,
-                            position: Int,
-                            id: Long
-                        ) {
-                            val questionSubject = parent.getItemAtPosition(position) as String
-
-                            //find course correponding to the selected name
-                            val course =
-                                coursesList.filter { course -> course.courseName == questionSubject }[0]
-                            if (user != null) {
-                                DatabaseManager.db.addQuestion(
-                                    user.userId,
-                                    course.courseId,
-                                    questTitle.toString(),
-                                    questBody.toString(),
-                                    imageURI.toString()
-                                )
-                            }
-                        }
-
-                        override fun onNothingSelected(parent: AdapterView<*>) {
-                            // Do nothing
-                        }
-                    }
-
-                    mainActivity.replaceFragment(HomeFragment(mainActivity), "HomeFragment")
-                }
-            }
-
-
-            //select image
-            val uploadButton = view?.findViewById<Button>(R.id.uploadButton)
-            uploadButton?.setOnClickListener {
-                pickImage.launch("image/*")
-            }
-
-
-            takePictureButton = view.findViewById(R.id.takeImage)
-
-            takePictureButton.setOnClickListener {
-                val questionDetails = questBody.text.toString()
-                val questionTitle = questTitle.text.toString()
-
-                val intent = Intent(this.mainActivity, CameraActivity::class.java)
-                intent.putExtra("questionTitle", questionTitle)
-                intent.putExtra("questionDetails", questionDetails)
-
-                startActivity(intent)
-            }
+            setSubmitButton(view, questBody, questTitle, spinner, coursesList, user, imageURI)
+            seUploadImage(view)
+            setTakeImage(view, questBody, questTitle)
         }
 
         return view
 
         }
+
+    private fun setSubmitButton(
+        view: View?,
+        questBody: EditText,
+        questTitle: EditText,
+        spinner: Spinner,
+        coursesList: List<Model.Course>,
+        user: Model.User?,
+        imageURI: TextView
+    ) {
+        val submitButton = view?.findViewById<Button>(R.id.btn_submit)
+        submitButton?.setOnClickListener {
+            print("click submit btn")
+
+            if (questBody.text.isBlank() || questTitle.text.isBlank()) {
+                Toast.makeText(
+                    requireContext(),
+                    "Question title or body cannot be empty",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parent: AdapterView<*>,
+                        view: View?,
+                        position: Int,
+                        id: Long
+                    ) {
+                        val questionSubject = parent.getItemAtPosition(position) as String
+                        //find course correponding to the selected name
+                        val course =
+                            coursesList.filter { course -> course.courseName == questionSubject }[0]
+                        if (user != null) {
+                            db.addQuestion(
+                                user.userId,
+                                course.courseId,
+                                questTitle.toString(),
+                                questBody.toString(),
+                                imageURI.toString()
+                            )
+                        }
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>) {}
+                }
+
+                mainActivity.replaceFragment(HomeFragment(mainActivity), "HomeFragment")
+            }
+        }
+    }
+
+    private fun setTakeImage(
+        view: View,
+        questBody: EditText,
+        questTitle: EditText
+    ) {
+        takePictureButton = view.findViewById(R.id.takeImage)
+
+        takePictureButton.setOnClickListener {
+            val questionDetails = questBody.text.toString()
+            val questionTitle = questTitle.text.toString()
+
+            val intent = Intent(this.mainActivity, CameraActivity::class.java)
+            intent.putExtra("questionTitle", questionTitle)
+            intent.putExtra("questionDetails", questionDetails)
+
+            startActivity(intent)
+        }
+    }
+
+    private fun seUploadImage(view: View?) {
+        val uploadButton = view?.findViewById<Button>(R.id.uploadButton)
+        uploadButton?.setOnClickListener {
+            pickImage.launch("image/*")
+        }
+    }
+
+    private fun setUp(view: View): Triple<EditText, EditText, TextView> {
+        val questBody = view.findViewById<EditText>(R.id.question_details_edittext)
+        val questTitle = view.findViewById<EditText>(R.id.question_title_edittext)
+        val imageURI = view.findViewById<TextView>(R.id.image_uri)
+
+        questBody.setText(this.mainActivity.intent.getStringExtra("questionDetails"))
+        questTitle.setText(this.mainActivity.intent.getStringExtra("questionTitle"))
+        imageURI.setText(this.mainActivity.intent.getStringExtra("uri"))
+        return Triple(questBody, questTitle, imageURI)
+    }
 }
