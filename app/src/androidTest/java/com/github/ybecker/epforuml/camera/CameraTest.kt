52930c7df04da8d014063bf532d99bfe7f65ea7f
unknown <luna.godier@epfl.ch>
2023-03-30 16:27:06 +0200

    testing camera

--- app/src/androidTest/java/com/github/ybecker/epforuml/camera/CameraTest.kt
+++ app/src/androidTest/java/com/github/ybecker/epforuml/camera/CameraTest.kt
@@ -1,28 +1,74 @@
 package com.github.ybecker.epforuml.camera
 
-import com.github.ybecker.epforuml.R
 import android.app.Activity
 import android.content.Intent
 import android.util.Log
+import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity
+import com.github.ybecker.epforuml.CameraActivity
 import com.github.ybecker.epforuml.MainActivity
-import com.github.ybecker.epforuml.authentication.LoginActivity
+import com.github.ybecker.epforuml.R
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
+import java.util.regex.Matcher
 
 @RunWith(AndroidJUnit4::class)
 class CameraTest {
 
+    @get:Rule
+    val activityRule = ActivityScenarioRule(CameraActivity::class.java)
+
+    @Before
+    fun beforeTest(){
+        Intents.init()
+    }
+    @After
+    fun afterTest(){
+        Intents.release()
+    }
     @Test
-    fun newQuestionSetsUpWhenIntentFilled(){
+    fun testCameraStartedWhenPermissionsGranted() {
+        // Given
+        if (allPermissionsGranted()) {
+            // When
+            onView(withId(R.id.viewFinder)).check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun testCameraNotStartedWhenPermissionsNotGranted() {
+        // Given
+        if (!allPermissionsGranted()) {
+            // When
+            onView(withText("Permissions not granted by the user.")).check(matches(isDisplayed()))
+            activityRule.scenario.close()
+        }
+    }
+
+
+    private fun allPermissionsGranted() = true
+
+    @Test
+    fun newQuestionSetsUpWhenIntentFilled() {
 
         val intent = Intent(
             ApplicationProvider.getApplicationContext(),
@@ -49,28 +95,5 @@ class CameraTest {
         } catch (e: Exception) {
             Log.e("NewQuestionFragment", "Error lauching activity: \${e.message}")
         }
-}/*
-
-    @Test
-    fun navigatesCorrectly(){
-        val scenario = ActivityScenario.launch(LoginActivity::class.java)
-        // go to MainActivity
-        onView(ViewMatchers.withId(R.id.guestButton)).perform(ViewActions.click())
-
-        onView(ViewMatchers.withId(R.id.home_layout_parent)).check(matches(ViewMatchers.isDisplayed()))
-
-        // open navigation drawer
-        onView(ViewMatchers.withContentDescription(R.string.open))
-            .perform(ViewActions.click())
-        onView(ViewMatchers.withId(R.id.nav_home)).perform(ViewActions.click())
-        onView(ViewMatchers.withId(R.id.new_question_button)).perform(ViewActions.click())
-        onView(ViewMatchers.withId(R.id.takeImage)).check(matches(ViewMatchers.isDisplayed()))
-        onView(ViewMatchers.withId(R.id.takeImage)).perform(ViewActions.scrollTo())
-        onView(ViewMatchers.withId(R.id.takeImage)).perform(ViewActions.click())
-
-        onView(ViewMatchers.withId(R.id.image_capture_button)).check(matches(ViewMatchers.isDisplayed()))
-        onView(ViewMatchers.withId(R.id.image_capture_button)).perform(ViewActions.click())
-
-
     }
-*/}
+}
