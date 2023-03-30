e2f73ff570904659bea15f3726066ff384b0ed0b
unknown <luna.godier@epfl.ch>
2023-03-30 12:21:03 +0200

    added test suite

--- app/src/androidTest/java/com/github/ybecker/epforuml/EditPhotoTest.kt
+++ app/src/androidTest/java/com/github/ybecker/epforuml/EditPhotoTest.kt
@@ -2,42 +2,31 @@ package com.github.ybecker.epforuml
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Color
-import androidx.compose.ui.test.hasClickAction
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.NoActivityResumedException
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity
 import com.github.ybecker.epforuml.authentication.LoginActivity
-import com.github.ybecker.epforuml.database.DatabaseManager
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import org.junit.After
-import org.junit.Assert.assertThrows
 import org.junit.Before
 import org.junit.Rule
-import org.junit.Test import org.junit.rules.ExpectedException
+import org.junit.Test
+import org.junit.rules.ExpectedException
 import org.junit.runner.RunWith
-import java.io.InputStream
 
-
-//@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4::class)
 class EditPhotoTest {
-/*
+
     lateinit var scenario: ActivityScenario<LoginActivity>
 
     @Before
     fun initTests() {
-        Firebase.auth.signOut()
-        DatabaseManager.user = null
         Intents.init()
     }
 
@@ -45,24 +34,27 @@ class EditPhotoTest {
     fun endTests() {
         Intents.release()
     }
-*/
 
-    /*@Test
-    fun goesBackToNewQuestionWhenDone(){
+    @Test
+    fun displaysEditorOnCorrectWorkflow(){
 
         scenario = ActivityScenario.launch(LoginActivity::class.java)
+
         // go to MainActivity
         onView(withId(R.id.guestButton)).perform(click())
-        onView(withContentDescription(R.string.open)).perform(click())
-        onView(withId(R.id.nav_home)).perform(click())
         onView(withId(R.id.new_question_button)).perform(click())
 
         onView(withId(R.id.takeImage)).perform(click())
 
         onView(withId(R.id.image_capture_button)).perform(click())
-
-        //intended(hasComponent(DsPhotoEditorActivity::class.java.name))
-        //onView(hasClickAction()).check(matches(isDisplayed()))
+       var done = false
+        while(!done){
+            try{
+                intended(hasComponent(DsPhotoEditorActivity::class.java.name))
+                done = true
+            } catch (e: AssertionError){
+            }
+        }
 
         //onView(withId(R.id.ds_photo_editor_top_button_apply)).perform(click())
 
@@ -70,7 +62,21 @@ class EditPhotoTest {
 
         //onView(withId(R.id.image_uri)).check(matches(isDisplayed()))
 
-    }*/
+    }
+    @Test
+    fun failsWithIncorrectUri(){
+        var exception = ExpectedException.none()
+        val intent = Intent(
+            ApplicationProvider.getApplicationContext(),
+            EditPhotoActivity::class.java
+        )
+
+        intent.putExtra("uri", "INVALIDURI")
+
+        ActivityScenario.launch<Activity>(intent)
+        exception.expect(IllegalArgumentException::class.java)
+
+    }
 
 
     }
