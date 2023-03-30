40d1ab9bb31339c71ea07792004c216d9c77b32f
unknown <luna.godier@epfl.ch>
2023-03-30 14:41:01 +0200

    fix: forgot to close scenarios

--- app/src/androidTest/java/com/github/ybecker/epforuml/EditPhotoTest.kt
+++ app/src/androidTest/java/com/github/ybecker/epforuml/EditPhotoTest.kt
@@ -12,43 +12,25 @@ import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity
-import com.github.ybecker.epforuml.authentication.LoginActivity
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.ExpectedException
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class EditPhotoTest {
-    private lateinit var scenario : ActivityScenario<MainActivity>
-
-    @Before
-    fun initTests() {
-        Intents.init()
-    }
-
-    @After
-    fun endTests() {
-        Intents.release()
-        scenario.close()
-    }
 
     @Test
     fun displaysEditorOnCorrectWorkflow(){
-
-
+        Intents.init()
         val intent = Intent(
             ApplicationProvider.getApplicationContext(),
             MainActivity::class.java
         )
-        scenario = ActivityScenario.launch(intent)
+        val scenario = ActivityScenario.launch<Activity>(intent)
 
         onView(withId(R.id.new_question_button)).perform(click())
         onView(withId(R.id.takeImage)).perform(click())
         onView(withId(R.id.image_capture_button)).perform(click())
-       var done = false
+        var done = false
         while(!done){
             try{
                 intended(hasComponent(DsPhotoEditorActivity::class.java.name))
@@ -56,5 +38,7 @@ class EditPhotoTest {
             } catch (e: AssertionError){
             }
         }
+        Intents.release()
+        scenario.close()
     }
 }
