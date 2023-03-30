dea1215f51f2088951e55e734adf048fd532aa2f
caroline.verchere <caroline.verchere@epfl.ch>
2023-03-30 16:47:21 +0200

    m

---
+++ app/src/main/java/com/github/ybecker/epforuml/MyQuestionsAdapter.kt
@@ -0,0 +1,40 @@
+package com.github.ybecker.epforuml
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.github.ybecker.epforuml.R
+import com.github.ybecker.epforuml.database.Model
+
+class MyQuestionsAdapter(private val myQuestionsMap: MutableMap<Model.Course, MutableList<Model.Question>>) : RecyclerView.Adapter<MyQuestionsAdapter.MyQuestionsViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyQuestionsViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.my_questions_item, parent, false)
+        return MyQuestionsViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return myQuestionsMap.size
+    }
+
+    override fun onBindViewHolder(holder: MyQuestionsViewHolder, position: Int) {
+        val course = myQuestionsMap.keys.elementAt(position)
+
+        holder.courseTitle.text = course.courseId
+
+        val questionsList = myQuestionsMap[course]
+
+        holder.forumAdapter = ForumAdapter(questionsList ?: mutableListOf())
+
+        holder.questionsRecyclerView.adapter = holder.forumAdapter
+    }
+
+    class MyQuestionsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val courseTitle: TextView = itemView.findViewById(R.id.course_title_text_view)
+        val questionsRecyclerView: RecyclerView = itemView.findViewById(R.id.recycler_my_questions)
+        lateinit var forumAdapter: ForumAdapter
+    }
+}
--- app/src/main/java/com/github/ybecker/epforuml/MyQuestionsFragment.kt
+++ app/src/main/java/com/github/ybecker/epforuml/MyQuestionsFragment.kt
@@ -1,10 +1,21 @@
 package com.github.ybecker.epforuml
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.github.ybecker.epforuml.MyQuestionsAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.auth.data.model.User
+import com.github.ybecker.epforuml.authentication.AuthenticatorManager
+import com.github.ybecker.epforuml.database.DatabaseManager
+import com.github.ybecker.epforuml.database.DatabaseManager.db
+import com.github.ybecker.epforuml.database.Model
+import java.util.concurrent.CompletableFuture
 
 
 /**
@@ -12,12 +23,97 @@ import android.view.ViewGroup
  * Use the [MyQuestionsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+
 class MyQuestionsFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: MyQuestionsAdapter
+    val user = AuthenticatorManager.authenticator?.user
+    private var myQuestionsMap = mutableMapOf<Model.Course, MutableList<Model.Question>>() // switch to questions when able to transfer data from mainActivtiy
+
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_questions, container, false)
+        val view = inflater.inflate(R.layout.fragment_my_questions, container, false)
+
+
+        //Uncomment
+        DatabaseManager.useMockDatabase()
+
+            return view
+        }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView = view.findViewById(R.id.recycler_my_questions)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true) // maybe change that later
+
+
+
+        if (user != null) {
+            // User is not logged in, display a message
+            val message = "You need to be logged in to view your questions."
+            val messageView = view.findViewById<TextView>(R.id.not_connected_text_view)
+            messageView.text = message
+            messageView.visibility = View.VISIBLE
+        } else {
+            refresh()
+        }
+
     }
+
+
+    private fun getMyQuestionsMap() {
+        //user?.let { user ->
+            val userId = "user1"
+            db.getUserQuestions(userId)
+                .thenAccept { questions ->
+                    myQuestionsMap = mutableMapOf<Model.Course, MutableList<Model.Question>>()
+                    questions.forEach { question ->
+                        db.getCourseById(question.courseId).thenAccept { course ->
+                            val courseQuestions = myQuestionsMap.getOrDefault(course, mutableListOf())
+                            course?.let {
+                                courseQuestions.add(question)
+                                myQuestionsMap[course] = courseQuestions
+                            }
+                        }
+                    }
+
+                }
+      //  }
+    }
+
+
+
+
+
+
+
+    private fun myQuestionsDisplay() {
+        adapter = MyQuestionsAdapter(myQuestionsMap)
+
+        recyclerView.adapter = adapter
+
+    // move to QuestionDetails when clicking on specific question
+    }
+
+    private fun refresh() {
+
+        getMyQuestionsMap()
+
+    }
+
+
 }
