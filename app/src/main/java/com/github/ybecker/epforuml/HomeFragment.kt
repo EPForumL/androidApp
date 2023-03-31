71922b0bfead624d0ff34b7f3168ca6416822e6b
Anouk <anouk.paulino@epfl.ch>
2023-03-31 11:03:03 +0200

    missing strings

--- app/src/main/java/com/github/ybecker/epforuml/HomeFragment.kt
+++ app/src/main/java/com/github/ybecker/epforuml/HomeFragment.kt
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.github.ybecker.epforuml.database.DatabaseManager.db
 import com.github.ybecker.epforuml.database.Model.*
 import android.widget.ImageButton
+import com.github.ybecker.epforuml.database.DatabaseManager
 import java.util.concurrent.CompletableFuture
 
 /**
@@ -48,7 +49,7 @@ class HomeFragment(private val mainActivity: MainActivity) : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        //DatabaseManager.useMockDatabase()
+        DatabaseManager.useMockDatabase()
 
         futureCourseList = db.availableCourses()
 
