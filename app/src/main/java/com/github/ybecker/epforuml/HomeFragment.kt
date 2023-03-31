caf0cbad02f8f2cfc39c1c9abf3dd037f6f2ad05
Anouk <anouk.paulino@epfl.ch>
2023-03-31 11:27:59 +0200

    fix

--- app/src/main/java/com/github/ybecker/epforuml/HomeFragment.kt
+++ app/src/main/java/com/github/ybecker/epforuml/HomeFragment.kt
@@ -49,7 +49,7 @@ class HomeFragment(private val mainActivity: MainActivity) : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        DatabaseManager.useMockDatabase()
+        //DatabaseManager.useMockDatabase()
 
         futureCourseList = db.availableCourses()
 
