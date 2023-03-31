8f3681630940226326dc3d198c179d6a1e8454a9
unknown <luna.godier@epfl.ch>
2023-03-31 10:25:15 +0200

    test: wrote small test suite for CameraActivity

--- app/src/androidTest/java/com/github/ybecker/epforuml/camera/CameraTest.kt
+++ app/src/androidTest/java/com/github/ybecker/epforuml/camera/CameraTest.kt
@@ -1,99 +1,44 @@
 package com.github.ybecker.epforuml.camera
 
-import android.app.Activity
-import android.content.Intent
-import android.util.Log
-import android.view.View
-import androidx.core.content.ContextCompat
 import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity
+import androidx.test.filters.LargeTest
 import com.github.ybecker.epforuml.CameraActivity
-import com.github.ybecker.epforuml.MainActivity
-import com.github.ybecker.epforuml.R
 import org.junit.After
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.regex.Matcher
 
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class CameraTest {
-
-    @get:Rule
-    val activityRule = ActivityScenarioRule(CameraActivity::class.java)
+    private lateinit var scenario : ActivityScenario<CameraActivity>
 
     @Before
-    fun beforeTest(){
-        Intents.init()
+    fun launchActivity() {
+        // Launch the CameraActivity
+        scenario = ActivityScenario.launch(CameraActivity::class.java)
     }
+
     @After
-    fun afterTest(){
-        Intents.release()
-    }
-    @Test
-    fun testCameraStartedWhenPermissionsGranted() {
-        // Given
-        if (allPermissionsGranted()) {
-            // When
-            onView(withId(R.id.viewFinder)).check(matches(isDisplayed()))
-        }
+    fun closeScenario() {
+        // close the CameraActivity
+        scenario.close()
+
     }
 
     @Test
-    fun testCameraNotStartedWhenPermissionsNotGranted() {
-        // Given
-        if (!allPermissionsGranted()) {
-            // When
-            onView(withText("Permissions not granted by the user.")).check(matches(isDisplayed()))
-            activityRule.scenario.close()
-        }
+    fun testCaptureButtonVisible() {
+        // Check if the capture button is visible
+        onView(withId(com.github.ybecker.epforuml.R.id.image_capture_button)).check(matches(isDisplayed()))
     }
 
-
-    private fun allPermissionsGranted() = true
-
     @Test
-    fun newQuestionSetsUpWhenIntentFilled() {
-
-        val intent = Intent(
-            ApplicationProvider.getApplicationContext(),
-            MainActivity::class.java
-        )
-        intent.putExtra("questionTitle", "Luna")
-        intent.putExtra("questionDetails", "Godier")
-
-        intent.putExtra("uri", "thisistheuri")
-
-
-        try {
-            ActivityScenario.launch<Activity>(intent)
-
-            onView(ViewMatchers.withContentDescription(R.string.open))
-                .perform(ViewActions.click())
-            onView(ViewMatchers.withId(R.id.nav_home)).perform(ViewActions.click())
-            onView(ViewMatchers.withId(R.id.new_question_button)).perform(ViewActions.click())
-
-            onView(ViewMatchers.withId(R.id.question_details_edittext)).check(matches(withText("Godier")))
-            onView(ViewMatchers.withId(R.id.question_title_edittext)).check(matches(withText("Luna")))
-            onView(ViewMatchers.withId(R.id.image_uri)).check(matches(withText("thisistheuri")))
-
-        } catch (e: Exception) {
-            Log.e("NewQuestionFragment", "Error lauching activity: \${e.message}")
-        }
+    fun testPreviewVisible() {
+        // Check if the preview is visible
+        onView(withId(com.github.ybecker.epforuml.R.id.viewFinder)).check(matches(isDisplayed()))
     }
 }
