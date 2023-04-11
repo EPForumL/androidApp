0c1fe562a9fe1bb9eb2e1e170b9b9753e91a8f12
Anouk <anouk.paulino@epfl.ch>
2023-04-11 10:08:44 +0200

    send button implementation

--- app/src/main/java/com/github/ybecker/epforuml/QuestionDetailsActivity.kt
+++ app/src/main/java/com/github/ybecker/epforuml/QuestionDetailsActivity.kt
@@ -3,47 +3,64 @@ package com.github.ybecker.epforuml
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.github.ybecker.epforuml.database.DatabaseManager.db
 import com.github.ybecker.epforuml.database.Model
 
 class QuestionDetailsActivity : AppCompatActivity() {
 
     private lateinit var answerAdapter : AnswerAdapter
     private lateinit var answerRecyclerView: RecyclerView
+    private var question = intent.getParcelableExtra<Model.Question>("question")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_details)
 
-        val originFragment = intent.getStringExtra("originFragment")
+        //val originFragment = intent.getStringExtra("originFragment")
 
         val button : Button = findViewById(R.id.back_to_forum_button)
         button.setOnClickListener{ // Create an intent to return to the previous fragment
             startActivity(Intent(applicationContext, MainActivity::class.java))
         }
 
-        val sendButton : CardView =  findViewById(R.id.post_reply_button)
-        sendButton.setOnClickListener(sendReply())
-
-
         answerRecyclerView = findViewById(R.id.answers_recycler)
         answerRecyclerView.layoutManager = LinearLayoutManager(this)
         answerRecyclerView.setHasFixedSize(true)
 
-        val question = intent.getParcelableExtra<Model.Question>("question")
         if (question != null) {
             val title : TextView = findViewById(R.id.qdetails_title)
-            title.text = question.questionTitle
+            title.text = question!!.questionTitle
 
-            answerRecyclerView.adapter = AnswerAdapter(question.questionId, question.questionText, question.answers)
+            updateRecycler(question!!)
+        }
+
+        val sendButton : CardView =  findViewById(R.id.post_reply_button)
+        val replyBox : EditText = findViewById(R.id.write_reply_box)
+        // store content of box as a new answer to corresponding question
+        sendButton.setOnClickListener {
+            if (question != null) {
+                val replyText : String =  replyBox.text.toString()
+                replyBox.setText("")
+
+                // TODO : retrieve userId
+                db.addAnswer("user", question!!.questionId, replyText)
+                updateRecycler(question!!)
+            } else {
+                Toast.makeText(applicationContext, "Could not load this question.", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
 
-    private fun sendReply() {
 
+    private fun updateRecycler(question : Model.Question) {
+        answerRecyclerView.adapter = AnswerAdapter(question.questionId, question.questionText, question.answers)
     }
 }
