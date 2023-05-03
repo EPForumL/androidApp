1f9716f3cd277078757bbd0daeba8b5dd48b05e0
unknown <luna.godier@epfl.ch>
2023-05-03 11:12:41 +0200

    tests fixed

--- app/src/androidTest/java/com/github/ybecker/epforuml/QuestionDetailsWithUri.kt
+++ app/src/androidTest/java/com/github/ybecker/epforuml/QuestionDetailsWithUri.kt
@@ -1,23 +1,31 @@
 /*
 package com.github.ybecker.epforuml
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.ybecker.epforuml.database.DatabaseManager
 import com.github.ybecker.epforuml.database.DatabaseManager.db
 import com.github.ybecker.epforuml.database.Model
+import junit.framework.TestCase
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.File
 
 @RunWith(AndroidJUnit4::class)
 class QuestionDetailsWithUri {
@@ -36,10 +44,34 @@ class QuestionDetailsWithUri {
     }
 
     @Test
-    fun questionWithStoresImageDisplaysCorrectly(){
-        onView(withText("IMAGE TEST")).perform(click())
-        scenario.onActivity {
-            assert(it.findViewById<ImageView>(R.id.image_question).visibility == View.VISIBLE)
+    fun submitMakesCorrectUpload() {
+
+        val intent = Intent(
+            ApplicationProvider.getApplicationContext(),
+            MainActivity::class.java
+        )
+        intent.putExtra("questionTitle", "Luna")
+        intent.putExtra("questionDetails", "Godier")
+        val file = File("com/github/ybecker/epforuml/IMG_1215 (1).JPG")
+        assert(file.exists())
+        val uri = Uri.fromFile(file)
+        intent.putExtra("uri", uri.toString())
+
+        try {
+            ActivityScenario.launch<Activity>(intent)
+
+            onView(withContentDescription(R.string.open))
+                .perform(click())
+            onView(withId(R.id.nav_home)).perform(click())
+            onView(withId(R.id.new_question_button)).perform(click())
+            onView(withId(R.id.btn_submit)).perform(scrollTo(), click())
+
+            onView(withText("Luna")).perform(click())
+            scenario.onActivity {
+                assert(it.findViewById<ImageView>(R.id.image_question).visibility==View.VISIBLE)
+            }
+        } catch (e: Exception) {
+            Log.e("NewQuestionFragment", "Error lauching activity: \${e.message}")
         }
     }
 
@@ -50,4 +82,5 @@ class QuestionDetailsWithUri {
     }
 
 
-}*/
+}
+*/
