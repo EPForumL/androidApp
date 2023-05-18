5750de9b0aa0c3a1169d8a92f3ba4f822631aa55
Anouk <anouk.paulino@epfl.ch>
2023-05-18 20:31:39 +0200

    ok EditPhotoTest

--- app/src/androidTest/java/com/github/ybecker/epforuml/EditPhotoTest.kt
+++ app/src/androidTest/java/com/github/ybecker/epforuml/EditPhotoTest.kt
@@ -4,8 +4,16 @@ import android.app.Activity
 import android.content.Intent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity
 import com.github.ybecker.epforuml.sensor.EditPhotoActivity
 import org.junit.Test
 import org.junit.rules.ExpectedException
