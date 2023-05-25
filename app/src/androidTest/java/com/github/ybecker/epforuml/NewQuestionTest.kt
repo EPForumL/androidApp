00b9e911ec8dc8df1354b1eae9225dffce8c8ed4
Yann <yann.becker@epfl.ch>
2023-05-25 10:13:15 +0200

    Fix testVoiceButtonDisplayed test

--- app/src/androidTest/java/com/github/ybecker/epforuml/NewQuestionTest.kt
+++ app/src/androidTest/java/com/github/ybecker/epforuml/NewQuestionTest.kt
@@ -460,9 +460,8 @@ class NewQuestionTest {
 
         //Scroll to the end of the page
         onView(withId(R.id.new_question_scrollview)).perform(ViewActions.swipeUp())
-        onView(withText("Record Voice Note")).check(matches(isDisplayed()))
-        onView(withText("Play_voice_note")).check(matches(isDisplayed()))
-
+        onView(withId(R.id.play_note_button)).check(matches(isDisplayed()))
+        onView(withId(R.id.voice_note_button)).check(matches(isDisplayed()))
     }
 
 
